dc81b0a5e137be69e54a7dafd23e62c01fdc5a72
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-09-04 21:51:40 +0100

    added tests for traveller model

---
+++ models/traveller.js
@@ -0,0 +1,52 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+      return journey.startLocation;
+  });
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  })
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  })
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0)
+};
+
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+//   const getModes = this.journeys.map((journey) => {
+//     return journey.transport;
+//   })
+//   const uniqueModes = getModes.filter((x, i, a) => a.indexOf(x) ==i);
+//   return uniqueModes;
+// };
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const getModes = this.journeys.map((journey) => {
+    return journey.transport;
+  })
+  const uniqueModes = [...new Set(getModes)]
+  return uniqueModes;
+};
+
+
+module.exports = Traveller;
